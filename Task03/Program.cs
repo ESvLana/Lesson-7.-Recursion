ce750362d0475bf2918f1205a4d096b0e95a5214
ESvLana <Sv9toshka@mail.ru>
2024-02-04 23:35:43 +0300

    Added Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] CreateRandomArray(int size)  // Задайте произвольный массив
+{
+    int[] Array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = rnd.Next(0, 100);
+    }
+    return Array;
+}
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] arr, int i)  // Выведете его элементы, начиная с конца
+    {
+        if (i <= 0)
+        {
+            return;
+        }
+        else
+        {
+            Console.Write(arr[i - 1] + " ");
+            ReverseArray(arr, i - 1);
+        }
+    }
+
+
+Console.WriteLine("Введите размер массива: ");
+int n = Int32.Parse(Console.ReadLine()!);
+
+int[] Array = CreateRandomArray(n);
+Console.WriteLine("Массив до разворота:");
+PrintArray(Array);
+
+Console.WriteLine("Массив после разворота:");
+ReverseArray(Array, n);
